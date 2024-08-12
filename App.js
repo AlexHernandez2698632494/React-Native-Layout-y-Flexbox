7543638c9be3bf4fa681334096d2b77917f81f67
AlexHernandez2698632494 <133897289+AlexHernandez2698632494@users.noreply.github.com>
2024-08-12 11:09:45 -0600

    Part I

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import { View, StyleSheet } from "react-native";
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.contenedor}>
+        <View style={styles.caja1}></View>
+        <View style={styles.caja2}></View>
+        <View style={styles.caja3}></View>
+        <View style={styles.caja4}></View>
+      </View>
+    </>
   );
-}
-
+};
 const styles = StyleSheet.create({
-  container: {
+  contenedor: {
+    backgroundColor: "cornflowerblue",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+  },
+  caja1: {
+    padding: 20,
+    backgroundColor: "navy",
+  },
+  caja2: {
+    padding: 20,
+    backgroundColor: "yellow",
+  },
+  caja3: {
+    padding: 20,
+    backgroundColor: "green",
+  },
+  caja4: {
+    padding: 20,
+    backgroundColor: "teal",
   },
 });
+export default App;
